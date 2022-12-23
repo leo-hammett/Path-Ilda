270f5aceee152e4c5cb8a10863a61f6ff26cc6db
leo hammett <leo@hammett.biz>
2022-12-23 17:21:00 +0000

    Began Constructing The Line Object

--- Path/Form1.cs
+++ Path/Form1.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Path
 {
     public partial class Form1 : Form
@@ -5,6 +7,20 @@ namespace Path
         public Form1()
         {
             InitializeComponent();
+        }
+        class PathLine
+        {
+            private string name;
+            public string Name
+            { get; set; }
+            private List<PathLineFrame> keyFrames;
+            public List<PathLineFrame> KeyFrames
+            { get; set; }
+        }
+        class PathLineFrame
+        {
+            int time; //Note the time is in frames not seconds, id recommend 30 frames per second unless you have expensive gear
+
         }
     }
 }
