859c9752a7ee321655d7a79d1a4187f8962d7221
leo hammett <leo@hammett.biz>
2022-12-23 16:37:39 +0000

    Minor Changes - More Buttons

--- Path/Form1.Designer.cs
+++ Path/Form1.Designer.cs
@@ -30,12 +30,24 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.timeLinePanel = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.TimeLineBackOneFrame = new System.Windows.Forms.Button();
             this.timeLineTitle = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.timeLinePanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,14 +64,80 @@
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Location = new System.Drawing.Point(530, 12);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(530, 490);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(394, 632);
+            this.panel1.Size = new System.Drawing.Size(394, 154);
             this.panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Load Path File";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(149, 71);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(145, 23);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(149, 42);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 23);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(3, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "KPPS (Points Per Second):";
+            // 
+            // label4
+            // 
+            this.label4.AccessibleName = "Time Manager";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(-2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 30);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Settings";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(3, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "FPS (Frames Per Second):";
             // 
             // timeLinePanel
             // 
             this.timeLinePanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.timeLinePanel.Controls.Add(this.textBox2);
+            this.timeLinePanel.Controls.Add(this.textBox1);
+            this.timeLinePanel.Controls.Add(this.label2);
+            this.timeLinePanel.Controls.Add(this.label1);
             this.timeLinePanel.Controls.Add(this.checkBox1);
             this.timeLinePanel.Controls.Add(this.button1);
             this.timeLinePanel.Controls.Add(this.TimeLineBackOneFrame);
@@ -68,6 +146,51 @@
             this.timeLinePanel.Name = "timeLinePanel";
             this.timeLinePanel.Size = new System.Drawing.Size(512, 114);
             this.timeLinePanel.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(305, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 23);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(305, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 23);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(211, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Time (Frames)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(211, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Time (Seconds)";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(305, 17);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(39, 25);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Play";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
@@ -91,23 +214,22 @@
             // 
             this.timeLineTitle.AccessibleName = "Time Manager";
             this.timeLineTitle.AutoSize = true;
+            this.timeLineTitle.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.timeLineTitle.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.timeLineTitle.Location = new System.Drawing.Point(3, 0);
             this.timeLineTitle.Name = "timeLineTitle";
-            this.timeLineTitle.Size = new System.Drawing.Size(83, 15);
+            this.timeLineTitle.Size = new System.Drawing.Size(147, 30);
             this.timeLineTitle.TabIndex = 0;
             this.timeLineTitle.Text = "Time Operator";
             // 
-            // checkBox1
+            // button3
             // 
-            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(305, 17);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(39, 25);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "Play";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(178, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Save Path File";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -122,6 +244,8 @@
             this.Name = "Form1";
             this.Text = "Path";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.timeLinePanel.ResumeLayout(false);
             this.timeLinePanel.PerformLayout();
             this.ResumeLayout(false);
@@ -138,5 +262,16 @@
         private Button TimeLineBackOneFrame;
         private Label timeLineTitle;
         private CheckBox checkBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private Button button2;
+        private Button button3;
     }
 }
