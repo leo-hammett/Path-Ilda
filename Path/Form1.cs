05cbda52d7dcf69ce8dcdfc98171462756779955
leo hammett <leo@hammett.biz>
2022-12-24 22:24:48 +0000

    Started work on the selection functions

--- Path/Form1.cs
+++ Path/Form1.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+//Point.distance is your friend.
 
 namespace Path
 {
@@ -10,16 +11,19 @@ namespace Path
         }
         class PathLine
         {
-            private string name;
-            public string Name
-            { get; set; }
-            private List<PathLineFrame> keyFrames; //A list of all keyframes sorted by time
-            public List<PathLineFrame> KeyFrames
-            { get; set; }
+            string name;
+            public string Name  { get; set; }
+            List<PathLineFrame> keyFrames; //A list of all keyframes sorted by time
+            public List<PathLineFrame> KeyFrames    { get; set; }
+            int listIndex;
+            public int ListIndex { get; set; }
             public int getValueXWayBetweenTwoPoints(int value1, int value2, float multiplier)
             {
                 return Convert.ToInt32(value1 + (value2 - value1) * multiplier);
             }
+
+            bool isHidden = false;
+            public bool IsHidden { get; set; }
             public PathLineFrame GenFrameAt(int time)
             {
                 PathLineFrame frameBefore;
@@ -57,13 +61,31 @@ namespace Path
                     //SET ALL PROPERTIES TO PROPERTIES IN BETWEEN THEM BOTH
                     float animationProgress = (time - frameBefore.Time) / (frameAfter.Time-frameBefore.Time); //If you set each property to beforeFrame value plus (afterFrame - beforeFrame) * difference  -- This assumes a linear animation hence the constant progress as time moves forward.
                     newFrame.PathColor = Color.FromArgb(
-                        getValueXWayBetweenTwoPoints(frameAfter.PathColor.A, frameBefore.PathColor.A, animationProgress),
-                        getValueXWayBetweenTwoPoints(frameAfter.PathColor.R, frameBefore.PathColor.R, animationProgress),
-                        getValueXWayBetweenTwoPoints(frameAfter.PathColor.G, frameBefore.PathColor.G, animationProgress),
-                        getValueXWayBetweenTwoPoints(frameAfter.PathColor.B, frameBefore.PathColor.B, animationProgress)); //I turned it into a function because I was bound to make a mistake.
+                        getValueXWayBetweenTwoPoints(frameBefore.PathColor.A, frameAfter.PathColor.A, animationProgress),
+                        getValueXWayBetweenTwoPoints(frameBefore.PathColor.R, frameAfter.PathColor.R, animationProgress),
+                        getValueXWayBetweenTwoPoints(frameBefore.PathColor.G, frameAfter.PathColor.G, animationProgress),
+                        getValueXWayBetweenTwoPoints(frameBefore.PathColor.B, frameAfter.PathColor.B, animationProgress)); //I turned it into a function because I was bound to make a mistake. And because I need to use this function a LOT.
+                    while (frameBefore.PathPoints.Count != frameAfter.PathPoints.Count)
+                    {
+                        if (frameBefore.PathPoints.Count < frameAfter.PathPoints.Count)
+                        {
+                            frameBefore.PathPoints.Add(frameBefore.PathPoints.Last());
+                        }
+                        else
+                        {
+                            frameAfter.PathPoints.Add(frameAfter.PathPoints.Last());
+                        }
+                    }   //Makes all extra detail grow out the end of the line --im proud of myself for the attention to detail, might be buggy tho
+                    for(int i = 0; i < frameBefore.PathPoints.Count;i++)
+                    {
+                        newFrame.PathPoints.Add(new Point(
+                            getValueXWayBetweenTwoPoints(frameBefore.PathPoints[i].X, frameAfter.PathPoints[i].X,animationProgress),
+                            getValueXWayBetweenTwoPoints(frameBefore.PathPoints[i].Y, frameAfter.PathPoints[i].Y,animationProgress)
+                            ));
+                    }
                     return newFrame;
                 }
-            }
+            }   //The majourity of the animation code IF THIS PROJECT DOESNT WORK IMMA CRY
         }
         class PathLineFrame
         {
@@ -74,6 +96,46 @@ namespace Path
             public Color PathColor { get; set; }
 
             List<Point> pathPoints;
+            public List<Point> PathPoints { get; set; }
+            int listIndex;
+            public int ListIndex { get; set; };
+            public List<LinePoint> GenKeyPoints(bool middle = false)
+            {
+                List<LinePoint> KeyPoints= new List<LinePoint>();
+                for(int i = 0; i < PathPoints.Count; i++)
+                {
+                    KeyPoints.Add(new LinePoint(ListIndex, pathPoints[i], false));
+                    if(middle && (i+1) != pathPoints.Count)
+                    {
+                        KeyPoints.Add(new LinePoint(ListIndex, 
+                            Convert.ToInt32((pathPoints[i].X + pathPoints[i+1].X) / 2),
+                            Convert.ToInt32((pathPoints[i].Y + pathPoints[i + 1].Y) / 2)
+                            ,true));
+                    }
+                }
+                return KeyPoints;
+            }   
+            /*So basically this function generates keypoints, keypoints are any points with details that we might want to adjust
+             * This selection tool should work well until there are a lot of points to select*/
+            
+        }
+        class LinePoint
+        {
+            int shapeListIndex;
+            Point location;
+            bool isMiddle;
+            public LinePoint(int ShapeListIndex, Point Location, bool IsMiddle)
+            {
+                this.shapeListIndex = ShapeListIndex;
+                this.location = Location;
+                this.isMiddle = IsMiddle;
+            }
+            public LinePoint(int ShapeListIndex, int X, int Y, bool IsMiddle)
+            {
+                this.shapeListIndex = ShapeListIndex;
+                this.location = new Point(X,Y);
+                this.isMiddle = IsMiddle;
+            }
         }
     }
 }
