93a0abc3c7ab8fc42b3ab4d9360dae3ef3ccd90f
leo hammett <leo@hammett.biz>
2022-12-25 19:59:32 +0000

    Added an information panel so I can debug more easily with graphics. THIS IS CHRISTMAS DAY JUST SAYING

--- Path/Form1.Designer.cs
+++ Path/Form1.Designer.cs
@@ -28,13 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PreviewGraphics = new System.Windows.Forms.PictureBox();
             this.OptionsPanel = new System.Windows.Forms.Panel();
+            this.OptionsDrawLineMode = new System.Windows.Forms.CheckBox();
+            this.OptionsSelectModeButton = new System.Windows.Forms.CheckBox();
+            this.OptionsEmergencyLabel = new System.Windows.Forms.Label();
+            this.OptionsToggleProject = new System.Windows.Forms.CheckBox();
+            this.OptionsSavePathFile = new System.Windows.Forms.Button();
             this.OptionsLoadPathFile = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.OptionsKPPSTextBox = new System.Windows.Forms.TextBox();
+            this.OptionsFPSTextBox = new System.Windows.Forms.TextBox();
             this.OptionsKPPSLabel = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.OptionsTitleLabel = new System.Windows.Forms.Label();
             this.OptionsFPSLabel = new System.Windows.Forms.Label();
             this.TimeLinePanel = new System.Windows.Forms.Panel();
             this.TimeLineFramesInput = new System.Windows.Forms.TextBox();
@@ -45,30 +50,28 @@
             this.TimeLineNextFrame = new System.Windows.Forms.Button();
             this.TimeLineBackFrame = new System.Windows.Forms.Button();
             this.TimeLineTitleLabel = new System.Windows.Forms.Label();
-            this.OptionsSavePathFile = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.LinePropertiesPanel = new System.Windows.Forms.Panel();
-            this.OptionsToggleProject = new System.Windows.Forms.CheckBox();
-            this.OptionsEmergencyLabel = new System.Windows.Forms.Label();
-            this.OptionsSelectModeButton = new System.Windows.Forms.CheckBox();
-            this.OptionsDrawLineMode = new System.Windows.Forms.CheckBox();
             this.LinePropertiesTitle = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.InformationPanel = new System.Windows.Forms.Panel();
+            this.InformationTitleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewGraphics)).BeginInit();
             this.OptionsPanel.SuspendLayout();
             this.TimeLinePanel.SuspendLayout();
             this.LinePropertiesPanel.SuspendLayout();
+            this.InformationPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // PreviewGraphics
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.PreviewGraphics.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PreviewGraphics.Location = new System.Drawing.Point(12, 12);
+            this.PreviewGraphics.Name = "PreviewGraphics";
+            this.PreviewGraphics.Size = new System.Drawing.Size(512, 512);
+            this.PreviewGraphics.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PreviewGraphics.TabIndex = 0;
+            this.PreviewGraphics.TabStop = false;
             // 
             // OptionsPanel
             // 
@@ -79,15 +82,67 @@
             this.OptionsPanel.Controls.Add(this.OptionsToggleProject);
             this.OptionsPanel.Controls.Add(this.OptionsSavePathFile);
             this.OptionsPanel.Controls.Add(this.OptionsLoadPathFile);
-            this.OptionsPanel.Controls.Add(this.textBox4);
-            this.OptionsPanel.Controls.Add(this.textBox3);
+            this.OptionsPanel.Controls.Add(this.OptionsKPPSTextBox);
+            this.OptionsPanel.Controls.Add(this.OptionsFPSTextBox);
             this.OptionsPanel.Controls.Add(this.OptionsKPPSLabel);
-            this.OptionsPanel.Controls.Add(this.label4);
+            this.OptionsPanel.Controls.Add(this.OptionsTitleLabel);
             this.OptionsPanel.Controls.Add(this.OptionsFPSLabel);
             this.OptionsPanel.Location = new System.Drawing.Point(530, 438);
             this.OptionsPanel.Name = "OptionsPanel";
             this.OptionsPanel.Size = new System.Drawing.Size(394, 206);
             this.OptionsPanel.TabIndex = 1;
+            // 
+            // OptionsDrawLineMode
+            // 
+            this.OptionsDrawLineMode.Appearance = System.Windows.Forms.Appearance.Button;
+            this.OptionsDrawLineMode.AutoSize = true;
+            this.OptionsDrawLineMode.Location = new System.Drawing.Point(82, 171);
+            this.OptionsDrawLineMode.Name = "OptionsDrawLineMode";
+            this.OptionsDrawLineMode.Size = new System.Drawing.Size(69, 25);
+            this.OptionsDrawLineMode.TabIndex = 17;
+            this.OptionsDrawLineMode.Text = "Draw Line";
+            this.OptionsDrawLineMode.UseVisualStyleBackColor = true;
+            // 
+            // OptionsSelectModeButton
+            // 
+            this.OptionsSelectModeButton.Appearance = System.Windows.Forms.Appearance.Button;
+            this.OptionsSelectModeButton.AutoSize = true;
+            this.OptionsSelectModeButton.Location = new System.Drawing.Point(3, 171);
+            this.OptionsSelectModeButton.Name = "OptionsSelectModeButton";
+            this.OptionsSelectModeButton.Size = new System.Drawing.Size(73, 25);
+            this.OptionsSelectModeButton.TabIndex = 16;
+            this.OptionsSelectModeButton.Text = "Select Tool";
+            this.OptionsSelectModeButton.UseVisualStyleBackColor = true;
+            // 
+            // OptionsEmergencyLabel
+            // 
+            this.OptionsEmergencyLabel.AutoSize = true;
+            this.OptionsEmergencyLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.OptionsEmergencyLabel.Location = new System.Drawing.Point(77, 133);
+            this.OptionsEmergencyLabel.Name = "OptionsEmergencyLabel";
+            this.OptionsEmergencyLabel.Size = new System.Drawing.Size(162, 15);
+            this.OptionsEmergencyLabel.TabIndex = 15;
+            this.OptionsEmergencyLabel.Text = "PRESS SPACE IN EMERGENCY";
+            // 
+            // OptionsToggleProject
+            // 
+            this.OptionsToggleProject.Appearance = System.Windows.Forms.Appearance.Button;
+            this.OptionsToggleProject.AutoSize = true;
+            this.OptionsToggleProject.Location = new System.Drawing.Point(3, 128);
+            this.OptionsToggleProject.Name = "OptionsToggleProject";
+            this.OptionsToggleProject.Size = new System.Drawing.Size(68, 25);
+            this.OptionsToggleProject.TabIndex = 14;
+            this.OptionsToggleProject.Text = "Laser On?";
+            this.OptionsToggleProject.UseVisualStyleBackColor = true;
+            // 
+            // OptionsSavePathFile
+            // 
+            this.OptionsSavePathFile.Location = new System.Drawing.Point(125, 99);
+            this.OptionsSavePathFile.Name = "OptionsSavePathFile";
+            this.OptionsSavePathFile.Size = new System.Drawing.Size(116, 23);
+            this.OptionsSavePathFile.TabIndex = 13;
+            this.OptionsSavePathFile.Text = "Save Path File";
+            this.OptionsSavePathFile.UseVisualStyleBackColor = true;
             // 
             // OptionsLoadPathFile
             // 
@@ -98,19 +153,19 @@
             this.OptionsLoadPathFile.Text = "Load Path File";
             this.OptionsLoadPathFile.UseVisualStyleBackColor = true;
             // 
-            // textBox4
+            // OptionsKPPSTextBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(149, 71);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(145, 23);
-            this.textBox4.TabIndex = 11;
+            this.OptionsKPPSTextBox.Location = new System.Drawing.Point(149, 71);
+            this.OptionsKPPSTextBox.Name = "OptionsKPPSTextBox";
+            this.OptionsKPPSTextBox.Size = new System.Drawing.Size(145, 23);
+            this.OptionsKPPSTextBox.TabIndex = 11;
             // 
-            // textBox3
+            // OptionsFPSTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(149, 42);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(145, 23);
-            this.textBox3.TabIndex = 9;
+            this.OptionsFPSTextBox.Location = new System.Drawing.Point(149, 42);
+            this.OptionsFPSTextBox.Name = "OptionsFPSTextBox";
+            this.OptionsFPSTextBox.Size = new System.Drawing.Size(145, 23);
+            this.OptionsFPSTextBox.TabIndex = 9;
             // 
             // OptionsKPPSLabel
             // 
@@ -122,17 +177,17 @@
             this.OptionsKPPSLabel.TabIndex = 10;
             this.OptionsKPPSLabel.Text = "KPPS (Points Per Second):";
             // 
-            // label4
+            // OptionsTitleLabel
             // 
-            this.label4.AccessibleName = "Time Manager";
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label4.Location = new System.Drawing.Point(-2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 30);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Options";
+            this.OptionsTitleLabel.AccessibleName = "Time Manager";
+            this.OptionsTitleLabel.AutoSize = true;
+            this.OptionsTitleLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OptionsTitleLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.OptionsTitleLabel.Location = new System.Drawing.Point(-2, 0);
+            this.OptionsTitleLabel.Name = "OptionsTitleLabel";
+            this.OptionsTitleLabel.Size = new System.Drawing.Size(86, 30);
+            this.OptionsTitleLabel.TabIndex = 9;
+            this.OptionsTitleLabel.Text = "Options";
             // 
             // OptionsFPSLabel
             // 
@@ -235,15 +290,6 @@
             this.TimeLineTitleLabel.TabIndex = 0;
             this.TimeLineTitleLabel.Text = "Time Operator";
             // 
-            // OptionsSavePathFile
-            // 
-            this.OptionsSavePathFile.Location = new System.Drawing.Point(125, 99);
-            this.OptionsSavePathFile.Name = "OptionsSavePathFile";
-            this.OptionsSavePathFile.Size = new System.Drawing.Size(116, 23);
-            this.OptionsSavePathFile.TabIndex = 13;
-            this.OptionsSavePathFile.Text = "Save Path File";
-            this.OptionsSavePathFile.UseVisualStyleBackColor = true;
-            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
@@ -254,51 +300,8 @@
             this.LinePropertiesPanel.Controls.Add(this.LinePropertiesTitle);
             this.LinePropertiesPanel.Location = new System.Drawing.Point(530, 12);
             this.LinePropertiesPanel.Name = "LinePropertiesPanel";
-            this.LinePropertiesPanel.Size = new System.Drawing.Size(394, 420);
+            this.LinePropertiesPanel.Size = new System.Drawing.Size(394, 208);
             this.LinePropertiesPanel.TabIndex = 3;
-            // 
-            // OptionsToggleProject
-            // 
-            this.OptionsToggleProject.Appearance = System.Windows.Forms.Appearance.Button;
-            this.OptionsToggleProject.AutoSize = true;
-            this.OptionsToggleProject.Location = new System.Drawing.Point(3, 128);
-            this.OptionsToggleProject.Name = "OptionsToggleProject";
-            this.OptionsToggleProject.Size = new System.Drawing.Size(68, 25);
-            this.OptionsToggleProject.TabIndex = 14;
-            this.OptionsToggleProject.Text = "Laser On?";
-            this.OptionsToggleProject.UseVisualStyleBackColor = true;
-            // 
-            // OptionsEmergencyLabel
-            // 
-            this.OptionsEmergencyLabel.AutoSize = true;
-            this.OptionsEmergencyLabel.ForeColor = System.Drawing.SystemColors.Control;
-            this.OptionsEmergencyLabel.Location = new System.Drawing.Point(77, 133);
-            this.OptionsEmergencyLabel.Name = "OptionsEmergencyLabel";
-            this.OptionsEmergencyLabel.Size = new System.Drawing.Size(162, 15);
-            this.OptionsEmergencyLabel.TabIndex = 15;
-            this.OptionsEmergencyLabel.Text = "PRESS SPACE IN EMERGENCY";
-            // 
-            // OptionsSelectModeButton
-            // 
-            this.OptionsSelectModeButton.Appearance = System.Windows.Forms.Appearance.Button;
-            this.OptionsSelectModeButton.AutoSize = true;
-            this.OptionsSelectModeButton.Location = new System.Drawing.Point(3, 171);
-            this.OptionsSelectModeButton.Name = "OptionsSelectModeButton";
-            this.OptionsSelectModeButton.Size = new System.Drawing.Size(73, 25);
-            this.OptionsSelectModeButton.TabIndex = 16;
-            this.OptionsSelectModeButton.Text = "Select Tool";
-            this.OptionsSelectModeButton.UseVisualStyleBackColor = true;
-            // 
-            // OptionsDrawLineMode
-            // 
-            this.OptionsDrawLineMode.Appearance = System.Windows.Forms.Appearance.Button;
-            this.OptionsDrawLineMode.AutoSize = true;
-            this.OptionsDrawLineMode.Location = new System.Drawing.Point(82, 171);
-            this.OptionsDrawLineMode.Name = "OptionsDrawLineMode";
-            this.OptionsDrawLineMode.Size = new System.Drawing.Size(69, 25);
-            this.OptionsDrawLineMode.TabIndex = 17;
-            this.OptionsDrawLineMode.Text = "Draw Line";
-            this.OptionsDrawLineMode.UseVisualStyleBackColor = true;
             // 
             // LinePropertiesTitle
             // 
@@ -312,6 +315,27 @@
             this.LinePropertiesTitle.TabIndex = 18;
             this.LinePropertiesTitle.Text = "Line Properties";
             // 
+            // InformationPanel
+            // 
+            this.InformationPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.InformationPanel.Controls.Add(this.InformationTitleLabel);
+            this.InformationPanel.Location = new System.Drawing.Point(530, 226);
+            this.InformationPanel.Name = "InformationPanel";
+            this.InformationPanel.Size = new System.Drawing.Size(394, 206);
+            this.InformationPanel.TabIndex = 19;
+            // 
+            // InformationTitleLabel
+            // 
+            this.InformationTitleLabel.AccessibleName = "Time Manager";
+            this.InformationTitleLabel.AutoSize = true;
+            this.InformationTitleLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.InformationTitleLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.InformationTitleLabel.Location = new System.Drawing.Point(3, 0);
+            this.InformationTitleLabel.Name = "InformationTitleLabel";
+            this.InformationTitleLabel.Size = new System.Drawing.Size(122, 30);
+            this.InformationTitleLabel.TabIndex = 18;
+            this.InformationTitleLabel.Text = "Information";
+            // 
             // Form1
             // 
             this.AccessibleName = "Path Window";
@@ -319,19 +343,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(936, 656);
+            this.Controls.Add(this.InformationPanel);
             this.Controls.Add(this.LinePropertiesPanel);
             this.Controls.Add(this.TimeLinePanel);
             this.Controls.Add(this.OptionsPanel);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.PreviewGraphics);
             this.Name = "Form1";
             this.Text = "Path";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewGraphics)).EndInit();
             this.OptionsPanel.ResumeLayout(false);
             this.OptionsPanel.PerformLayout();
             this.TimeLinePanel.ResumeLayout(false);
             this.TimeLinePanel.PerformLayout();
             this.LinePropertiesPanel.ResumeLayout(false);
             this.LinePropertiesPanel.PerformLayout();
+            this.InformationPanel.ResumeLayout(false);
+            this.InformationPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -339,17 +366,17 @@
 
         #endregion
 
-        private PictureBox pictureBox1;
+        private PictureBox PreviewGraphics;
         private Panel OptionsPanel;
         private Panel TimeLinePanel;
         private Button TimeLineNextFrame;
         private Button TimeLineBackFrame;
         private Label TimeLineTitleLabel;
         private CheckBox TimeLinePlay;
-        private TextBox textBox4;
-        private TextBox textBox3;
+        private TextBox OptionsKPPSTextBox;
+        private TextBox OptionsFPSTextBox;
         private Label OptionsKPPSLabel;
-        private Label label4;
+        private Label OptionsTitleLabel;
         private Label OptionsFPSLabel;
         private TextBox TimeLineFramesInput;
         private TextBox TimeLineSecondsInput;
@@ -365,5 +392,7 @@
         private SaveFileDialog saveFileDialog1;
         private Panel LinePropertiesPanel;
         private Label LinePropertiesTitle;
+        private Panel InformationPanel;
+        private Label InformationTitleLabel;
     }
 }
