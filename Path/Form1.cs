360bb4a8b78a76c25cae54ca04a9e5c7e2c8afe3
leo hammett <leo@hammett.biz>
2022-12-25 19:10:16 +0000

    Tidied Up?

--- Path/Form1.cs
+++ Path/Form1.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
-//Point.distance is your friend.
+//dynamicPath = the file & whole animation
+//framePath = the PathLineFrame array for the individual frame
 
 namespace Path
 {
@@ -13,25 +14,21 @@ namespace Path
         {
             string name;
             public string Name  { get; set; }
+
             List<PathLineFrame> keyFrames; //A list of all keyframes sorted by time
             public List<PathLineFrame> KeyFrames    { get; set; }
-            int listIndex;
-            public int ListIndex { get; set; }
-            public int getValueXWayBetweenTwoPoints(int value1, int value2, float multiplier)
-            {
-                return Convert.ToInt32(value1 + (value2 - value1) * multiplier);
-            }
+
+            int dynamicPathIndex;
+            public int DynamicPathIndex { get; set; }
 
             bool isHidden = false;
             public bool IsHidden { get; set; }
+
+            
             public PathLineFrame GenFrameAt(int time)
             {
-                PathLineFrame frameBefore;
-                PathLineFrame frameAfter;
-                frameAfter = new PathLineFrame();
-                frameAfter.Time = -1;                       //Set the time to -1 so we can tell if no frames before or after were found
-                frameBefore = new PathLineFrame();
-                frameBefore.Time = -1;                      //^^^ (I am sure theres an easier way to do this.
+                int frameBeforeIndex = -1;
+                int frameAfterIndex = -1;
                 for (int i = 0; i < keyFrames.Count; i++)
                 {
                     if (keyFrames[i].Time == time)          //Checks to see if the time lands on a keyframe - This is merely a performance thing
@@ -40,52 +37,39 @@ namespace Path
                     }
                     else if (keyFrames[i].Time < time)      //Finds the latest KeyFrame before the time
                     {
-                        frameBefore = keyFrames[i];
+                        PathLineFrame frameBefore = keyFrames[i];
                     }
-                    else if (frameAfter.Time == -1)         //Finds the first keyframe after the time. Gonna be real theres bound to be a logic error here.
+                    else if (frameAfterIndex == -1)         //Finds the first keyframe after the time. Gonna be real theres bound to be a logic error here.
                     {
-                        frameAfter = keyFrames[i];
+                        PathLineFrame frameAfter = keyFrames[i];
                     }
                 }
-                if (frameBefore.Time == -1)                  //If before all frames then newframe is the same as the frame after or first frame
+                if (frameBeforeIndex == -1)                  //If before all frames then newframe is the same as the frame after or first frame
                 {
-                    return frameAfter;
+                    return KeyFrames[frameAfterIndex];
                 }
-                else if (frameAfter.Time == -1)                   //Literally the same situation as the above if
+                else if (frameAfterIndex == -1)                   //Literally the same situation as the above if
                 {
-                    return frameBefore;
+                    return KeyFrames[frameBeforeIndex];
                 }
                 else
                 {
-                    PathLineFrame newFrame = new PathLineFrame();
+                    PathLineFrame frameAfter = keyFrames[frameAfterIndex];
+                    PathLineFrame frameBefore = keyFrames[frameBeforeIndex];
+                    
                     //SET ALL PROPERTIES TO PROPERTIES IN BETWEEN THEM BOTH
                     float animationProgress = (time - frameBefore.Time) / (frameAfter.Time-frameBefore.Time); //If you set each property to beforeFrame value plus (afterFrame - beforeFrame) * difference  -- This assumes a linear animation hence the constant progress as time moves forward.
-                    newFrame.PathColor = Color.FromArgb(
-                        getValueXWayBetweenTwoPoints(frameBefore.PathColor.A, frameAfter.PathColor.A, animationProgress),
-                        getValueXWayBetweenTwoPoints(frameBefore.PathColor.R, frameAfter.PathColor.R, animationProgress),
-                        getValueXWayBetweenTwoPoints(frameBefore.PathColor.G, frameAfter.PathColor.G, animationProgress),
-                        getValueXWayBetweenTwoPoints(frameBefore.PathColor.B, frameAfter.PathColor.B, animationProgress)); //I turned it into a function because I was bound to make a mistake. And because I need to use this function a LOT.
-                    while (frameBefore.PathPoints.Count != frameAfter.PathPoints.Count)
-                    {
-                        if (frameBefore.PathPoints.Count < frameAfter.PathPoints.Count)
-                        {
-                            frameBefore.PathPoints.Add(frameBefore.PathPoints.Last());
-                        }
-                        else
-                        {
-                            frameAfter.PathPoints.Add(frameAfter.PathPoints.Last());
-                        }
-                    }   //Makes all extra detail grow out the end of the line --im proud of myself for the attention to detail, might be buggy tho
-                    for(int i = 0; i < frameBefore.PathPoints.Count;i++)
-                    {
-                        newFrame.PathPoints.Add(new Point(
-                            getValueXWayBetweenTwoPoints(frameBefore.PathPoints[i].X, frameAfter.PathPoints[i].X,animationProgress),
-                            getValueXWayBetweenTwoPoints(frameBefore.PathPoints[i].Y, frameAfter.PathPoints[i].Y,animationProgress)
-                            ));
-                    }
+                    
                     return newFrame;
                 }
             }   //The majourity of the animation code IF THIS PROJECT DOESNT WORK IMMA CRY
+            public PathLine(string Name, PathLineFrame KeyFrame, int DynamicPathIndex)
+            { //For ListIndex just do Path
+                name = Name;
+                keyFrames.Add(KeyFrame);
+                dynamicPathIndex = DynamicPathIndex;
+            }
+            public PathLine(string Name, )
         }
         class PathLineFrame
         {
@@ -97,6 +81,7 @@ namespace Path
 
             List<Point> pathPoints;
             public List<Point> PathPoints { get; set; }
+            public void AddPoint(Point NewPoint) { pathPoints.Add(NewPoint) }
             int listIndex;
             public int ListIndex { get; set; };
             public List<LinePoint> GenKeyPoints(bool middle = false)
@@ -117,13 +102,72 @@ namespace Path
             }   
             /*So basically this function generates keypoints, keypoints are any points with details that we might want to adjust
              * This selection tool should work well until there are a lot of points to select*/
-            
+            public int getValueXWayBetweenTwoPoints(int value1, int value2, float multiplier)
+            {
+                return Convert.ToInt32(value1 + (value2 - value1) * multiplier);
+            } //This is a function to work out the properties of a nonkeyframe. Iz guud.
+            public PathLineFrame(int Time, Color PathColor, List<Point> PathPoints, int ListIndex)
+            {
+                time = Time;
+                pathColor = PathColor;
+                pathPoints = PathPoints;
+                listIndex = ListIndex;
+            }   //Basic constructor
+            public PathLineFrame(int Time, Color PathColor, Point Point1, Point Point2, int ListIndex)
+            {
+                time = Time;
+                pathColor = PathColor;
+                pathPoints = new List<Point>();
+                pathPoints.Add(Point1);
+                pathPoints.Add(Point2);
+                listIndex = ListIndex;
+            }   //Mid Constructor
+            public PathLineFrame(int Time, Color PathColor, int Point1x, int Point1y, int Point2x, int Point2y, int ListIndex)
+            {
+                time = Time;
+                pathColor = PathColor;
+                pathPoints = new List<Point>();
+                pathPoints.Add(new Point(Point1x,Point1y));
+                pathPoints.Add(new Point(Point2x,Point2y));
+                listIndex = ListIndex;
+            }   //Easy Constructor
+            public PathLineFrame(float AnimationProgress, PathLineFrame FrameBefore, PathLineFrame FrameAfter)
+            {
+                time = getValueXWayBetweenTwoPoints(FrameBefore.Time, FrameAfter.Time, AnimationProgress);
+                pathColor = Color.FromArgb(
+                        getValueXWayBetweenTwoPoints(FrameBefore.PathColor.A, FrameAfter.PathColor.A, AnimationProgress),
+                        getValueXWayBetweenTwoPoints(FrameBefore.PathColor.R, FrameAfter.PathColor.R, AnimationProgress),
+                        getValueXWayBetweenTwoPoints(FrameBefore.PathColor.G, FrameAfter.PathColor.G, AnimationProgress),
+                        getValueXWayBetweenTwoPoints(FrameBefore.PathColor.B, FrameAfter.PathColor.B, AnimationProgress)); //I turned it into a function because I was bound to make a mistake. And because I need to use this function a LOT.
+                while (FrameBefore.pathPoints.Count != FrameAfter.PathPoints.Count)
+                {
+                    if (FrameBefore.pathPoints.Count < FrameAfter.PathPoints.Count)
+                    {
+                        FrameAfter.pathPoints.Add(FrameBefore.PathPoints.Last());
+                    }
+                    else
+                    {
+                        FrameBefore.pathPoints.Add(FrameAfter.PathPoints.Last());
+                    }
+                }   //Makes all extra detail grow out the end of the line --im proud of myself for the attention to detail, might be buggy tho
+                for (int i = 0; i < pathPoints.Count; i++)
+                {
+                    pathPoints.Add(new Point(
+                        getValueXWayBetweenTwoPoints(FrameBefore.pathPoints[i].X, FrameAfter.pathPoints[i].X, AnimationProgress),
+                        getValueXWayBetweenTwoPoints(FrameBefore.pathPoints[i].Y, FrameAfter.pathPoints[i].Y, AnimationProgress)
+                        ));
+                }
+            }
         }
         class LinePoint
         {
             int shapeListIndex;
             Point location;
             bool isMiddle;
+            public PathLineFrame GetLineFrame()
+            {
+                return new PathLineFrame(0, Color.Black, 0, 0, 0, 0, 0);    //Make this return a line with the keypoint as point1 and the non keypoint as point2
+            }
             public LinePoint(int ShapeListIndex, Point Location, bool IsMiddle)
             {
                 this.shapeListIndex = ShapeListIndex;
@@ -136,6 +180,7 @@ namespace Path
                 this.location = new Point(X,Y);
                 this.isMiddle = IsMiddle;
             }
+
         }
     }
 }
